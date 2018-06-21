8a0df297e4014dc7a9bb168e45fa3bea2de0bd06
skurzynowski <sebastian.kurzynowski@gmail.com>
2018-06-21 16:15:37 +0200

    add timer

--- src/index.js
+++ src/index.js
@@ -3,45 +3,57 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import ButtonGroup from "react-bootstrap/lib/ButtonGroup";
 import Button from "react-bootstrap/lib/Button";
-import Jumbotron from "react-bootstrap/lib/Jumbotron";
 
 
 class Counter extends React.Component {
     render() {
-        return (<Button bsStyle="primary" disabled>Remebmerd:{this.props.counts}</Button>);
+        return (<Button bsStyle="primary" disabled>Remebmerd: {this.props.counts}</Button>);
     }
 }
 
 class Time extends React.Component {
+    changeTimeFormat = () => {
+        var minutes = Math.floor((this.props.time / 60));
+        var seconds = Math.floor((this.props.time % 60));
+        //prevent 1: 6
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+
+        return minutes + ':' + seconds;
+    };
+
     render() {
-        return (<Button bsStyle="primary" disabled>Time:{this.props.time}</Button>);
+        return (<Button bsStyle="primary" disabled>Training time: {this.changeTimeFormat()}</Button>);
     }
 }
 
-class Stats_Panel extends React.Component {
+class StatsPanel extends React.Component {
     render() {
         return (<ButtonGroup className="stats_panel">
-            <Counter counts="0"/>
-            <Time time="0:00"/>
+            <Counter counts={this.props.rememberd}/>
+            <Time time={this.props.time}/>
         </ButtonGroup>);
     }
 }
 
 
-class Presentation_Panel extends React.Component {
+class PresentationPanel extends React.Component {
     render() {
         return (
             <div className="subject">
-                5
+                {this.props.subject}
             </div>
-        )
-            ;
+        );
     }
 }
 
-class Next_Subject extends React.Component {
+class NextSubject extends React.Component {
     render() {
-        return (<Button bsStyle="primary">Next</Button>);
+        return (<Button bsStyle="primary" onClick={(e) => this.props.click(e)}>Next</Button>);
     }
 }
 
@@ -51,13 +63,12 @@ class Check extends React.Component {
     }
 }
 
-class Control_Panel extends React.Component {
+class ControlPanel extends React.Component {
     render() {
-
         return (
             <div className="control_panel">
                 <ButtonGroup>
-                    <Next_Subject/>
+                    <NextSubject click={this.props.click_next}/>
                     <Check/>
                 </ButtonGroup>
             </div>);
@@ -65,11 +76,39 @@ class Control_Panel extends React.Component {
 }
 
 class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rememberd: 0,
+            saved_numbers: [],
+            object: 0,
+            time: 0
+        };
+    }
+
+    componentDidMount = () => {
+        var timer = setInterval(
+            () => this.thick(),
+            1000);
+    };
+
+    thick = () => {
+        this.setState({time: this.state.time + 1});
+    };
+
+    click_next_subject = (e) => {
+        this.setState({
+            saved_numbers: this.state.saved_numbers.concat([this.state.object]),
+            object: Math.floor((Math.random() * 10)),
+            rememberd: this.state.saved_numbers.length + 1
+        });
+    };
+
     render() {
         return (<div className="game">
-            <Stats_Panel/>
-            <Presentation_Panel/>
-            <Control_Panel/>
+            <StatsPanel rememberd={this.state.rememberd} time={this.state.time}/>
+            <PresentationPanel subject={this.state.object}/>
+            <ControlPanel click_next={this.click_next_subject}/>
         </div>);
     }
 }
